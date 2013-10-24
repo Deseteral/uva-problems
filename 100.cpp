e5bc96554f000aa28575a614acc11823fba2afe4
Deseteral <deseteral@gmail.com>
2013-10-24 18:22:17 +0200

    Added UVa 100

---
+++ 100.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+
+int main()
+{
+	unsigned int i, j;
+	
+	while (scanf_s("%d %d", &i, &j) != EOF)
+	{
+		unsigned int a, b;
+		unsigned int n = 0;
+		unsigned int max = 0;
+
+		if (i < j)
+		{
+			a = i;
+			b = j;
+		}
+		else
+		{
+			a = j;
+			b = i;
+		}
+
+		for (unsigned int it = a; it <= b; it++)
+		{
+			n = it;
+
+			unsigned int cycle = 1;
+			while (n != 1)
+			{
+				if (n % 2 == 0)
+				{
+					n = n/2;
+					cycle++;
+				}
+				else
+				{
+					n = (3*n +1) /2;
+					cycle += 2;
+				}
+			}
+
+			if (cycle > max)
+				max = cycle;
+		}
+
+		printf("%d %d %d\n", i, j, max);
+	}
+	return 0;
+}
